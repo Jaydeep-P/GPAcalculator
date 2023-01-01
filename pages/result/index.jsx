a5b019590954faa615ce184f98628d639288dfaa
Hrushikesh Patel <hrushikesh592002@gmail.com>
2023-01-01 15:38:37 +0530

    styling

--- pages/result/index.jsx
+++ pages/result/index.jsx
@@ -5,7 +5,7 @@ import styles from "../../styles/Result.module.css";
 import homeStyles from "../../styles/Home.module.css";
 import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
+import Head from "next/head";
 const theme = createTheme({
   palette: {
     primary: {
@@ -19,35 +19,51 @@ const theme = createTheme({
 
 function index() {
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <div className={homeStyles.navbar}>
-          <div className={homeStyles.logoContainer}>
-            <div className={homeStyles.logo}>
-              <Image src="logo.svg" fill />
-            </div>
-          </div>
-        </div>
-        <div className={styles.container}>
-          <div className={styles.tile} style={{ height: "10rem" }}>
-            <div className={styles.gpaContainer}>
-              Your Gpa is <div style={{ fontSize: "5rem" }}>9.9</div>
-            </div>
-          </div>
-          <div className={styles.tile} style={{ flexDirection: "column" }}>
-            <div className={styles.chartContainer}>
-              <Chart />
-            </div>
-            <div className={styles.rankContainer}>Your score is above 50%</div>
-          </div>
-          <div className={styles.backButtonContainer}>
-            <Button href="/" variant="contained" className={styles.backButton}>
-              Back
-            </Button>
+    <>
+      <div className={homeStyles.navbar}>
+        <div className={homeStyles.logoContainer}>
+          <div className={homeStyles.logo}>
+            <Image src="logo.svg" fill />
           </div>
         </div>
       </div>
-    </ThemeProvider>
+      <div className={styles.wrapper}>
+        <Head>
+          <title>GPA Calculator</title>
+          <meta name="description" content="Simple GPA calculator" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <div>
+            <div className={styles.container}>
+              <div className={styles.tile} style={{ height: "10rem" }}>
+                <div className={styles.gpaContainer}>
+                  Your Gpa is <div style={{ fontSize: "5rem" }}>9.9</div>
+                </div>
+              </div>
+              <div className={styles.tile} style={{ flexDirection: "column" }}>
+                <div className={styles.chartContainer}>
+                  <Chart />
+                </div>
+                <div className={styles.rankContainer}>
+                  Your score is above 50%
+                </div>
+              </div>
+              <div className={styles.backButtonContainer}>
+                <Button
+                  href="/"
+                  variant="contained"
+                  className={styles.backButton}
+                >
+                  Back
+                </Button>
+              </div>
+            </div>
+          </div>
+        </ThemeProvider>
+      </div>
+    </>
   );
 }
 
